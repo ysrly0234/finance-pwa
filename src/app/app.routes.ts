be287bda4cd8efa05ca3436068f7145ef768ac26
Israel <ysrly0234@gmail.com>
2026-02-05 13:58:25 +0200

    s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { authGuard } from '../shared/guards/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 
+
 export const routes: Routes = [
     { path: '', component: OverviewComponent, canActivate: [authGuard] },
     { path: 'budgets', component: ManageBudgetsComponent, canActivate: [authGuard] },
